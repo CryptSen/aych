d3349229a397490680d9cc226c2fdf1c28aba57c
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-02 06:12:43 +0700

    Update autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-# Copyright (c) 2013-2016 The Bitcoin Core developers
+# Copyright (c) 2013 - 2020 The Bitcoin Core developers
+# Copyright (c) 2015 - 2020 The AYCHDeveloper
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
