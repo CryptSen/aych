7b153e94849858caa5865eb85d8cd2068f7503fe
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-03 02:45:46 +0700

    Update netmessagemaker.h

--- src/netmessagemaker.h
+++ src/netmessagemaker.h
@@ -1,10 +1,11 @@
-// Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2018 The Bitcoin Core developers
-// Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// Copyright (c) 2015 - 2020 Jean Wallet
+// Copyright (c) 2015 - 2020 The AYCHDeveloper
+// Distributed under the MIT software license, the AGPL-3.0 or later, see the accompanying
+// file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or https://www.gnu.org/licenses.
 
-#ifndef BITCOIN_NETMESSAGEMAKER_H
-#define BITCOIN_NETMESSAGEMAKER_H
+#ifndef AYCH_NETMESSAGEMAKER_H
+#define AYCH_NETMESSAGEMAKER_H
 
 #include <net.h>
 #include <serialize.h>
@@ -33,4 +34,4 @@ private:
     const int nVersion;
 };
 
-#endif // BITCOIN_NETMESSAGEMAKER_H
+#endif // AYCH_NETMESSAGEMAKER_H
