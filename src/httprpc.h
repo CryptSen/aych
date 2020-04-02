95668c69b7c4b0c470712e2c6f77dec0edc94e7c
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-02 16:17:36 +0700

    Update httprpc.h

--- src/httprpc.h
+++ src/httprpc.h
@@ -1,9 +1,11 @@
-// Copyright (c) 2015-2018 The Bitcoin Core developers
-// Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// Copyright (c) 2015 - 2020 Jean Wallet
+// Copyright (c) 2015 - 2020 The AYCHDeveloper
+// Distributed under the MIT software license, the AGPL-3.0 or later, see the accompanying
+// file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or https://www.gnu.org/licenses.
 
-#ifndef BITCOIN_HTTPRPC_H
-#define BITCOIN_HTTPRPC_H
+#ifndef AYCH_HTTPRPC_H
+#define AYCH_HTTPRPC_H
 
 #include <string>
 #include <map>
