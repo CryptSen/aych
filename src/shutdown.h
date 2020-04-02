bbc7dcf73210530cad1134dfa9a6645f22a9354a
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-03 06:43:27 +0700

    Update shutdown.h

--- src/shutdown.h
+++ src/shutdown.h
@@ -1,10 +1,11 @@
-// Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2018 The Bitcoin Core developers
-// Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// Copyright (c) 2015 - 2020 Jean Wallet
+// Copyright (c) 2015 - 2020 The AYCHDeveloper
+// Distributed under the MIT software license, the AGPL-3.0 or later, see the accompanying
+// file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or https://www.gnu.org/licenses.
 
-#ifndef BITCOIN_SHUTDOWN_H
-#define BITCOIN_SHUTDOWN_H
+#ifndef AYCH_SHUTDOWN_H
+#define AYCH_SHUTDOWN_H
 
 void StartShutdown();
 void AbortShutdown();
