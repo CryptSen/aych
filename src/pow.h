ae52addb8a6aecb45d8da7b30a175c6de1644ae6
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-03 02:58:36 +0700

    Update pow.h

--- src/pow.h
+++ src/pow.h
@@ -1,10 +1,11 @@
-// Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2018 The Bitcoin Core developers
-// Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// Copyright (c) 2015 - 2020 Jean Wallet
+// Copyright (c) 2015 - 2020 The AYCHDeveloper
+// Distributed under the MIT software license, the AGPL-3.0 or later, see the accompanying
+// file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or https://www.gnu.org/licenses.
 
-#ifndef BITCOIN_POW_H
-#define BITCOIN_POW_H
+#ifndef AYCH_POW_H
+#define AYCH_POW_H
 
 #include <consensus/params.h>
 
@@ -20,4 +21,4 @@ unsigned int CalculateNextWorkRequired(const CBlockIndex* pindexLast, int64_t nF
 /** Check whether a block hash satisfies the proof-of-work requirement specified by nBits */
 bool CheckProofOfWork(uint256 hash, unsigned int nBits, const Consensus::Params&);
 
-#endif // BITCOIN_POW_H
+#endif // AYCH_POW_H
