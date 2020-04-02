a2e1cd3b85f2d94e373c2060ec00e16d29c4a243
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-03 06:50:08 +0700

    Update streams.h

--- src/streams.h
+++ src/streams.h
@@ -1,10 +1,11 @@
-// Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2018 The Bitcoin Core developers
-// Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// Copyright (c) 2015 - 2020 Jean Wallet
+// Copyright (c) 2015 - 2020 The AYCHDeveloper
+// Distributed under the MIT software license, the AGPL-3.0 or later, see the accompanying
+// file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or https://www.gnu.org/licenses.
 
-#ifndef BITCOIN_STREAMS_H
-#define BITCOIN_STREAMS_H
+#ifndef AYCH_STREAMS_H
+#define AYCH_STREAMS_H
 
 #include <support/allocators/zeroafterfree.h>
 #include <serialize.h>
@@ -705,4 +706,4 @@ public:
     }
 };
 
-#endif // BITCOIN_STREAMS_H
+#endif // AYCH_STREAMS_H
