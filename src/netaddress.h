2a64bc76fd91ed8e9f7e713c2cc88320b7cbe9ba
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-03 02:32:17 +0700

    Update netaddress.h

--- src/netaddress.h
+++ src/netaddress.h
@@ -1,9 +1,11 @@
-// Copyright (c) 2009-2018 The Bitcoin Core developers
-// Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// Copyright (c) 2015 - 2020 Jean Wallet
+// Copyright (c) 2015 - 2020 The AYCHDeveloper
+// Distributed under the MIT software license, the AGPL-3.0 or later, see the accompanying
+// file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or https://www.gnu.org/licenses.
 
-#ifndef BITCOIN_NETADDRESS_H
-#define BITCOIN_NETADDRESS_H
+#ifndef AYCH_NETADDRESS_H
+#define AYCH_NETADDRESS_H
 
 #if defined(HAVE_CONFIG_H)
 #include <config/bitcoin-config.h>
@@ -172,4 +174,4 @@ class CService : public CNetAddr
         }
 };
 
-#endif // BITCOIN_NETADDRESS_H
+#endif // AYCH_NETADDRESS_H
