16ab117f2ecce57b7575c7df4ddf5b8547107b15
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-02 16:00:39 +0700

    Update dbwrapper.h

--- src/dbwrapper.h
+++ src/dbwrapper.h
@@ -1,9 +1,11 @@
-// Copyright (c) 2012-2018 The Bitcoin Core developers
-// Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// Copyright (c) 2015 - 2020 Jean Wallet
+// Copyright (c) 2015 - 2020 The AYCHDeveloper
+// Distributed under the MIT software license, the AGPL-3.0 or later, see the accompanying
+// file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or https://www.gnu.org/licenses.
 
-#ifndef BITCOIN_DBWRAPPER_H
-#define BITCOIN_DBWRAPPER_H
+#ifndef AYCH_DBWRAPPER_H
+#define AYCH_DBWRAPPER_H
 
 #include <clientversion.h>
 #include <fs.h>
@@ -349,4 +351,4 @@ public:
 
 };
 
-#endif // BITCOIN_DBWRAPPER_H
+#endif // AYCH_DBWRAPPER_H
