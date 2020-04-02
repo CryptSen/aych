004f5f9b156cf727db7e46baa9c09f89d7000b12
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-02 18:43:39 +0700

    Update net.h

--- src/net.h
+++ src/net.h
@@ -1,10 +1,11 @@
-// Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2018 The Bitcoin Core developers
-// Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// Copyright (c) 2015 - 2020 Jean Wallet
+// Copyright (c) 2015 - 2020 The AYCHDeveloper
+// Distributed under the MIT software license, the AGPL-3.0 or later, see the accompanying
+// file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or https://www.gnu.org/licenses.
 
-#ifndef BITCOIN_NET_H
-#define BITCOIN_NET_H
+#ifndef AYCH_NET_H
+#define AYCH_NET_H
 
 #include <addrdb.h>
 #include <addrman.h>
@@ -876,4 +877,4 @@ public:
 /** Return a timestamp in the future (in microseconds) for exponentially distributed events. */
 int64_t PoissonNextSend(int64_t now, int average_interval_seconds);
 
-#endif // BITCOIN_NET_H
+#endif // AYCH_NET_H
