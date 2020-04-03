dc1f66aa82b1b8d6b35120c274899f3f6639fb46
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-03 12:26:48 +0700

    Update scrypt.cpp

--- src/crypto/scrypt.cpp
+++ src/crypto/scrypt.cpp
@@ -24,7 +24,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * This file was originally written by Colin Percival as part of the Tarsnap
+ * This file was revised by Jean Wallet (from Colin Percival as part of the Tarsnap).
  * online backup system.
  */
 
