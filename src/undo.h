629c23001994c6671d531a41fb363eca5a357784
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-03 08:00:55 +0700

    Update undo.h

--- src/undo.h
+++ src/undo.h
@@ -1,10 +1,11 @@
-// Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2018 The Bitcoin Core developers
-// Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// Copyright (c) 2015 - 2020 Jean Wallet
+// Copyright (c) 2015 - 2020 The AYCHDeveloper
+// Distributed under the MIT software license, the AGPL-3.0 or later, see the accompanying
+// file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or https://www.gnu.org/licenses.
 
-#ifndef BITCOIN_UNDO_H
-#define BITCOIN_UNDO_H
+#ifndef AYCH_UNDO_H
+#define AYCH_UNDO_H
 
 #include <compressor.h>
 #include <consensus/consensus.h>
@@ -109,4 +110,4 @@ public:
     }
 };
 
-#endif // BITCOIN_UNDO_H
+#endif // AYCH_UNDO_H
