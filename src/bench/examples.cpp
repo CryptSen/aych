93ff10a54762bf8e5529b57a10292f122374d48d
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-03 10:47:53 +0700

    Update examples.cpp

--- src/bench/examples.cpp
+++ src/bench/examples.cpp
@@ -1,6 +1,8 @@
-// Copyright (c) 2015-2018 The Bitcoin Core developers
-// Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// Copyright (c) 2015 - 2020 Jean Wallet
+// Copyright (c) 2015 - 2020 The AYCHDeveloper
+// Distributed under the MIT software license, the AGPL-3.0 or later, see the accompanying
+// file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or https://www.gnu.org/licenses.
 
 #include <bench/bench.h>
 #include <validation.h>
