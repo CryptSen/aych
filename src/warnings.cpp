2c38e6d90d6af7ae0c123ee68ce8bfbe827f2c84
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-03 10:03:40 +0700

    Update warnings.cpp

--- src/warnings.cpp
+++ src/warnings.cpp
@@ -1,7 +1,8 @@
 // Copyright (c) 2015 - 2020 Jean Wallet
 // Copyright (c) 2015 - 2020 The AYCHDeveloper
-// Distributed under the MIT software license, see the accompanying
+// Distributed under the MIT software license, the AGPL-3.0 or later, see the accompanying
 // file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or https://www.gnu.org/licenses.
 
 #include <sync.h>
 #include <clientversion.h>
