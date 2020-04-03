4917ae823517430b3751a15426d8109aac0a1e30
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-03 12:42:48 +0700

    Update sha256_avx2.cpp

--- src/crypto/sha256_avx2.cpp
+++ src/crypto/sha256_avx2.cpp
@@ -1,3 +1,9 @@
+# Copyrighted (c) 2015 - 2020 Jean Wallet
+# Copyright (c) 2015 - 2020 The AYCHDeveloper
+# Distributed under the MIT software license, the AGPL-3.0 or later, see the accompanying
+# file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+# file LICENSE or https://www.gnu.org/licenses.
+
 #ifdef ENABLE_AVX2
 
 #include <stdint.h>
