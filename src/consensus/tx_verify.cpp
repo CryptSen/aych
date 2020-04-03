094bffae0367766840e6fbda8c8ca73ddc34631f
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-03 11:39:47 +0700

    Update tx_verify.cpp

--- src/consensus/tx_verify.cpp
+++ src/consensus/tx_verify.cpp
@@ -1,6 +1,8 @@
-// Copyright (c) 2017-2017 The Bitcoin Core developers
-// Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// Copyright (c) 2015 - 2020 Jean Wallet
+// Copyright (c) 2015 - 2020 The AYCHDeveloper
+// Distributed under the MIT software license, the AGPL-3.0 or later, see the accompanying
+// file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or https://www.gnu.org/licenses.
 
 #include <consensus/tx_verify.h>
 
