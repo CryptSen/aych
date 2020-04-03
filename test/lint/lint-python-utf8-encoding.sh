c4ff7792e5640d32bd468464421bbdc9dce99a53
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-03 17:05:50 +0700

    Update lint-python-utf8-encoding.sh

--- test/lint/lint-python-utf8-encoding.sh
+++ test/lint/lint-python-utf8-encoding.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 #
-# Copyright (c) 2018 The Bitcoin Core developers
-# Distributed under the MIT software license, see the accompanying
-# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+# Copyright (c) 2015 - 2020 Jean Wallet
+# Copyright (c) 2015 - 2020 The AYCHDeveloper
+# Distributed under the MIT software license, the AGPL-3.0 or later, see the accompanying
+# file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+# file LICENSE or https://www.gnu.org/licenses.
 #
 # Make sure we explicitly open all text files using UTF-8 (or ASCII) encoding to
 # avoid potential issues on the BSDs where the locale is not always set.
