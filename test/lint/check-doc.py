b2e67a3687be6e8ca021231ddad3d0105e7dfb42
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-03 16:51:02 +0700

    Update check-doc.py

--- test/lint/check-doc.py
+++ test/lint/check-doc.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
-# Copyright (c) 2015-2018 The Bitcoin Core developers
-# Distributed under the MIT software license, see the accompanying
-# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+# Copyright (c) 2015 - 2020 Jean Wallet
+# Copyright (c) 2015 - 2020 The AYCHDeveloper
+# Distributed under the MIT software license, the AGPL-3.0 or later, see the accompanying
+# file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+# file LICENSE or https://www.gnu.org/licenses.
 
 '''
 This checks if all command line args are documented.
