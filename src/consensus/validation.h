82814a11f7aba12ecabb5f4381074c37fbfbdc91
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-03 11:29:28 +0700

    Update validation.h

--- src/consensus/validation.h
+++ src/consensus/validation.h
@@ -1,10 +1,11 @@
-// Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2018 The Bitcoin Core developers
-// Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// Copyright (c) 2015 - 2020 Jean Wallet
+// Copyright (c) 2015 - 2020 The AYCHDeveloper
+// Distributed under the MIT software license, the AGPL-3.0 or later, see the accompanying
+// file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or https://www.gnu.org/licenses.
 
-#ifndef BITCOIN_CONSENSUS_VALIDATION_H
-#define BITCOIN_CONSENSUS_VALIDATION_H
+#ifndef AYCH_CONSENSUS_VALIDATION_H
+#define AYCH_CONSENSUS_VALIDATION_H
 
 #include <string>
 #include <version.h>
@@ -107,4 +108,4 @@ static inline int64_t GetTransactionInputWeight(const CTxIn& txin)
     return ::GetSerializeSize(txin, SER_NETWORK, PROTOCOL_VERSION | SERIALIZE_TRANSACTION_NO_WITNESS) * (WITNESS_SCALE_FACTOR - 1) + ::GetSerializeSize(txin, SER_NETWORK, PROTOCOL_VERSION) + ::GetSerializeSize(txin.scriptWitness.stack, SER_NETWORK, PROTOCOL_VERSION);
 }
 
-#endif // BITCOIN_CONSENSUS_VALIDATION_H
+#endif // AYCH_CONSENSUS_VALIDATION_H
