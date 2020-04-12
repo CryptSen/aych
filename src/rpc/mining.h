d56a1674571403b03c973293f1c5acab40c82810
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-12 10:50:02 +0700

    Update mining.h

--- src/rpc/mining.h
+++ src/rpc/mining.h
@@ -1,9 +1,11 @@
-// Copyright (c) 2017 The Bitcoin Core developers
-// Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// Copyright (c) 2015 - 2020 Jean Wallet
+// Copyright (c) 2015 - 2020 The AYCHDeveloper.
+// Distributed under the MIT software license, the SGPL-3.0+, see the accompanying
+// file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or https://www.gnu.org/licenses.
 
-#ifndef BITCOIN_RPC_MINING_H
-#define BITCOIN_RPC_MINING_H
+#ifndef AYCH_RPC_MINING_H
+#define AYCH_RPC_MINING_H
 
 #include <script/script.h>
 
