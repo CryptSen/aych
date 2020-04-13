a152969cb98fdbde1ef1b7cc4c831b3c9cb038b3
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-13 16:00:13 +0700

    Update and rename src/qt/bitcoinamountfield.h to src/aychamountfield.h

--- src/aychamountfield.h
+++ src/aychamountfield.h
@@ -1,9 +1,11 @@
-// Copyright (c) 2011-2018 The Bitcoin Core developers
+// Copyright (c) 2015 - 2020 Jean Wallet
+// Copyright (c) 2015 - 2020 The AYCHDeveloper.
 // Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or https://www.gnu.org/licenses.
 
-#ifndef BITCOIN_QT_BITCOINAMOUNTFIELD_H
-#define BITCOIN_QT_BITCOINAMOUNTFIELD_H
+#ifndef AYCH_QT_AYCHAMOUNTFIELD_H
+#define AYCH_QT_AYCHAMOUNTFIELD_H
 
 #include <amount.h>
 
@@ -72,4 +74,4 @@ private Q_SLOTS:
 
 };
 
-#endif // BITCOIN_QT_BITCOINAMOUNTFIELD_H
+#endif // AYCH_QT_AYCHAMOUNTFIELD_H
