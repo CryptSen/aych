1d335d07f3df68f35ccadd72f76a26b4d0adff4f
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-13 13:35:44 +0700

    Update rpcwallet.h

--- src/wallet/rpcwallet.h
+++ src/wallet/rpcwallet.h
@@ -1,9 +1,11 @@
-// Copyright (c) 2016-2018 The Bitcoin Core developers
+// Copyright (c) 2015 - 2020 Jean Wallet
+// Copyright (c) 2015 - 2020 The AYCHDeveloper.
 // Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or https://www.gnu.org/licenses.
 
-#ifndef BITCOIN_WALLET_RPCWALLET_H
-#define BITCOIN_WALLET_RPCWALLET_H
+#ifndef AYCH_WALLET_RPCWALLET_H
+#define AYCH_WALLET_RPCWALLET_H
 
 #include <string>
 
@@ -31,4 +33,4 @@ bool EnsureWalletIsAvailable(CWallet *, bool avoidException);
 UniValue getaddressinfo(const JSONRPCRequest& request);
 UniValue signrawtransactionwithwallet(const JSONRPCRequest& request);
 bool FillPSBT(const CWallet* pwallet, PartiallySignedTransaction& psbtx, int sighash_type = 1 /* SIGHASH_ALL */, bool sign = true, bool bip32derivs = false);
-#endif //BITCOIN_WALLET_RPCWALLET_H
+#endif //AYCH_WALLET_RPCWALLET_H
