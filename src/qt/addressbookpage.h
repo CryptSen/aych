1a13bf62791bc02103eb840196af590e3b7584bf
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-14 06:50:13 +0700

    Update addressbookpage.h

--- src/qt/addressbookpage.h
+++ src/qt/addressbookpage.h
@@ -1,9 +1,11 @@
-// Copyright (c) 2011-2018 The Bitcoin Core developers
+// Copyright (c) 2015 - 2020 Jean Wallet
+// Copyright (c) 2015 - 2020 The AYCHDeveloper.
 // Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or https://www.gnu.org/licenses.
 
-#ifndef BITCOIN_QT_ADDRESSBOOKPAGE_H
-#define BITCOIN_QT_ADDRESSBOOKPAGE_H
+#ifndef AYCH_QT_ADDRESSBOOKPAGE_H
+#define AYCH_QT_ADDRESSBOOKPAGE_H
 
 #include <QDialog>
 
@@ -83,4 +85,4 @@ Q_SIGNALS:
     void sendCoins(QString addr);
 };
 
-#endif // BITCOIN_QT_ADDRESSBOOKPAGE_H
+#endif // AYCH_QT_ADDRESSBOOKPAGE_H
