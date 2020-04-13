47b7ef540d7156cb45617403be15cb7b44d2486d
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-13 13:41:14 +0700

    Update wallet.h

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -1,10 +1,11 @@
-// Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2018 The Bitcoin Core developers
+// Copyright (c) 2015 - 2020 Jean Wallet
+// Copyright (c) 2015 - 2020 The AYCHDeveloper.
 // Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or https://www.gnu.org/licenses.
 
-#ifndef BITCOIN_WALLET_WALLET_H
-#define BITCOIN_WALLET_WALLET_H
+#ifndef AYCH_WALLET_WALLET_H
+#define AYCH_WALLET_WALLET_H
 
 #include <amount.h>
 #include <outputtype.h>
@@ -1319,4 +1320,4 @@ public:
 // be IsAllFromMe).
 int64_t CalculateMaximumSignedTxSize(const CTransaction &tx, const CWallet *wallet, bool use_max_sig = false);
 int64_t CalculateMaximumSignedTxSize(const CTransaction &tx, const CWallet *wallet, const std::vector<CTxOut>& txouts, bool use_max_sig = false);
-#endif // BITCOIN_WALLET_WALLET_H
+#endif // AYCH_WALLET_WALLET_H
