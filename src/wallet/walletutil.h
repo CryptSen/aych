8e082940638af0b121079722a7b0f8f405d433b4
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-13 15:28:23 +0700

    Update walletutil.h

--- src/wallet/walletutil.h
+++ src/wallet/walletutil.h
@@ -1,9 +1,11 @@
-// Copyright (c) 2017-2018 The Bitcoin Core developers
+// Copyright (c) 2015 - 2020 Jean Wallet
+// Copyright (c) 2015 - 2020 The AYCHDeveloper.
 // Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or https://www.gnu.org/licenses.
 
-#ifndef BITCOIN_WALLET_WALLETUTIL_H
-#define BITCOIN_WALLET_WALLETUTIL_H
+#ifndef AYCH_WALLET_WALLETUTIL_H
+#define AYCH_WALLET_WALLETUTIL_H
 
 #include <chainparamsbase.h>
 #include <util.h>
@@ -11,4 +13,4 @@
 //! Get the path of the wallet directory.
 fs::path GetWalletDir();
 
-#endif // BITCOIN_WALLET_WALLETUTIL_H
+#endif // AYCH_WALLET_WALLETUTIL_H
