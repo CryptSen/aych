c6b8a708fa3237d0d311154196f8bd0f11b6b134
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-13 08:20:56 +0700

    Update ismine.h

--- src/script/ismine.h
+++ src/script/ismine.h
@@ -1,10 +1,11 @@
-// Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2018 The Bitcoin Core developers
+// Copyright (c) 2015 - 2020 Jean Wallet
+// Copyright (c) 2015 - 2020 The AYCHDeveloper.
 // Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or https://www.gnu.org/licenses.
 
-#ifndef BITCOIN_SCRIPT_ISMINE_H
-#define BITCOIN_SCRIPT_ISMINE_H
+#ifndef AYCH_SCRIPT_ISMINE_H
+#define AYCH_SCRIPT_ISMINE_H
 
 #include <script/standard.h>
 
@@ -27,4 +28,4 @@ typedef uint8_t isminefilter;
 isminetype IsMine(const CKeyStore& keystore, const CScript& scriptPubKey);
 isminetype IsMine(const CKeyStore& keystore, const CTxDestination& dest);
 
-#endif // BITCOIN_SCRIPT_ISMINE_H
+#endif // AYCH_SCRIPT_ISMINE_H
