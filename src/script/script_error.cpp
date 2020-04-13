0aa1db3f0cbe6a6b4c7d9c4d2f2fbe5512a3cf44
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-13 08:27:36 +0700

    Update script_error.cpp

--- src/script/script_error.cpp
+++ src/script/script_error.cpp
@@ -1,7 +1,8 @@
-// Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2018 The Bitcoin Core developers
+// Copyright (c) 2015 - 2020 Jean Wallet
+// Copyright (c) 2015 - 2020 The AYCHDeveloper.
 // Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or https://www.gnu.org/licenses.
 
 #include <script/script_error.h>
 
