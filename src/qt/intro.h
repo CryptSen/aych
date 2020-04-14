53f1beb456d11681d6e0823f3f2ca8efd8927319
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-14 12:35:18 +0700

    Update intro.h

--- src/qt/intro.h
+++ src/qt/intro.h
@@ -1,9 +1,11 @@
-// Copyright (c) 2011-2018 The Bitcoin Core developers
+// Copyright (c) 2015 - 2020 Jean Wallet
+// Copyright (c) 2015 - 2020 The AYCHDeveloper.
 // Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or https://www.gnu.org/licenses.
 
-#ifndef BITCOIN_QT_INTRO_H
-#define BITCOIN_QT_INTRO_H
+#ifndef AYCH_QT_INTRO_H
+#define AYCH_QT_INTRO_H
 
 #include <QDialog>
 #include <QMutex>
@@ -79,4 +81,4 @@ private:
     friend class FreespaceChecker;
 };
 
-#endif // BITCOIN_QT_INTRO_H
+#endif // AYCH_QT_INTRO_H
