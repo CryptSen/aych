e557120075f275009a622884f303877b44386c35
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-14 07:04:40 +0700

    Update askpassphrasedialog.h

--- src/qt/askpassphrasedialog.h
+++ src/qt/askpassphrasedialog.h
@@ -1,9 +1,11 @@
-// Copyright (c) 2011-2018 The Bitcoin Core developers
+// Copyright (c) 2015 - 2020 Jean Wallet
+// Copyright (c) 2015 - 2020 The AYCHDeveloper.
 // Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or https://www.gnu.org/licenses.
 
-#ifndef BITCOIN_QT_ASKPASSPHRASEDIALOG_H
-#define BITCOIN_QT_ASKPASSPHRASEDIALOG_H
+#ifndef AYCH_QT_ASKPASSPHRASEDIALOG_H
+#define AYCH_QT_ASKPASSPHRASEDIALOG_H
 
 #include <QDialog>
 
@@ -50,4 +52,4 @@ protected:
     bool eventFilter(QObject *object, QEvent *event);
 };
 
-#endif // BITCOIN_QT_ASKPASSPHRASEDIALOG_H
+#endif // AYCH_QT_ASKPASSPHRASEDIALOG_H
