b383fb8ec36135b0579d59a76c97e506d0e36475
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-14 15:04:48 +0700

    Update signverifymessagedialog.h

--- src/qt/signverifymessagedialog.h
+++ src/qt/signverifymessagedialog.h
@@ -1,9 +1,11 @@
-// Copyright (c) 2011-2015 The Bitcoin Core developers
+// Copyright (c) 2015 - 2020 Jean Wallet
+// Copyright (c) 2015 - 2020 The AYCHDeveloper.
 // Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or https://www.gnu.org/licenses.
 
-#ifndef BITCOIN_QT_SIGNVERIFYMESSAGEDIALOG_H
-#define BITCOIN_QT_SIGNVERIFYMESSAGEDIALOG_H
+#ifndef AYCH_QT_SIGNVERIFYMESSAGEDIALOG_H
+#define AYCH_QT_SIGNVERIFYMESSAGEDIALOG_H
 
 #include <QDialog>
 
@@ -50,4 +52,4 @@ private Q_SLOTS:
     void on_clearButton_VM_clicked();
 };
 
-#endif // BITCOIN_QT_SIGNVERIFYMESSAGEDIALOG_H
+#endif // AYCH_QT_SIGNVERIFYMESSAGEDIALOG_H
