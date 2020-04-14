74102567deca538e337bc20e9aa9a68e954d4ed0
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-14 12:36:55 +0700

    Update macdockiconhandler.h

--- src/qt/macdockiconhandler.h
+++ src/qt/macdockiconhandler.h
@@ -1,9 +1,11 @@
-// Copyright (c) 2011-2015 The Bitcoin Core developers
+// Copyright (c) 2015 - 2020 Jean Wallet
+// Copyright (c) 2015 - 2020 The AYCHDeveloper.
 // Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or https://www.gnu.org/licenses.
 
-#ifndef BITCOIN_QT_MACDOCKICONHANDLER_H
-#define BITCOIN_QT_MACDOCKICONHANDLER_H
+#ifndef AYCH_QT_MACDOCKICONHANDLER_H
+#define AYCH_QT_MACDOCKICONHANDLER_H
 
 #include <QMainWindow>
 #include <QObject>
@@ -41,4 +43,4 @@ private:
     QMainWindow *mainWindow;
 };
 
-#endif // BITCOIN_QT_MACDOCKICONHANDLER_H
+#endif // AYCH_QT_MACDOCKICONHANDLER_H
