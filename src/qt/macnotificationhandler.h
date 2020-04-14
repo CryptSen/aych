5eea7c1f83a38e5779d4c9aeff4f96bf9bce86a0
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-14 12:39:44 +0700

    Update macnotificationhandler.h

--- src/qt/macnotificationhandler.h
+++ src/qt/macnotificationhandler.h
@@ -1,9 +1,11 @@
-// Copyright (c) 2011-2018 The Bitcoin Core developers
+// Copyright (c) 2015 - 2020 Jean Wallet
+// Copyright (c) 2015 - 2020 The AYCHDeveloper.
 // Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or https://www.gnu.org/licenses.
 
-#ifndef BITCOIN_QT_MACNOTIFICATIONHANDLER_H
-#define BITCOIN_QT_MACNOTIFICATIONHANDLER_H
+#ifndef AYCH_QT_MACNOTIFICATIONHANDLER_H
+#define AYCH_QT_MACNOTIFICATIONHANDLER_H
 
 #include <QObject>
 
@@ -24,4 +26,4 @@ public:
 };
 
 
-#endif // BITCOIN_QT_MACNOTIFICATIONHANDLER_H
+#endif // AYCH_QT_MACNOTIFICATIONHANDLER_H
