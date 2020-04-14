15c6a4c77df01d6fecaa02a1ddd2cd1f6059346c
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-14 14:04:13 +0700

    Update platformstyle.h

--- src/qt/platformstyle.h
+++ src/qt/platformstyle.h
@@ -1,9 +1,11 @@
-// Copyright (c) 2015 The Bitcoin Core developers
+// Copyright (c) 2015 - 2020 Jean Wallet
+// Copyright (c) 2015 - 2020 The AYCHDeveloper.
 // Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or https://www.gnu.org/licenses.
 
-#ifndef BITCOIN_QT_PLATFORMSTYLE_H
-#define BITCOIN_QT_PLATFORMSTYLE_H
+#ifndef AYCH_QT_PLATFORMSTYLE_H
+#define AYCH_QT_PLATFORMSTYLE_H
 
 #include <QIcon>
 #include <QPixmap>
@@ -51,5 +53,5 @@ private:
     /* ... more to come later */
 };
 
-#endif // BITCOIN_QT_PLATFORMSTYLE_H
+#endif // AYCH_QT_PLATFORMSTYLE_H
 
