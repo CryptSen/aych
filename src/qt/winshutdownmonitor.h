8f9b47d88c3387c31015da9da6e02d53411bcaec
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-14 16:24:24 +0700

    Update winshutdownmonitor.h

--- src/qt/winshutdownmonitor.h
+++ src/qt/winshutdownmonitor.h
@@ -1,9 +1,11 @@
-// Copyright (c) 2014-2018 The Bitcoin Core developers
+// Copyright (c) 2015 - 2020 Jean Wallet
+// Copyright (c) 2015 - 2020 The AYCHDeveloper.
 // Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or https://www.gnu.org/licenses.
 
-#ifndef BITCOIN_QT_WINSHUTDOWNMONITOR_H
-#define BITCOIN_QT_WINSHUTDOWNMONITOR_H
+#ifndef AYCH_QT_WINSHUTDOWNMONITOR_H
+#define AYCH_QT_WINSHUTDOWNMONITOR_H
 
 #ifdef WIN32
 #include <QByteArray>
@@ -24,4 +26,4 @@ public:
 };
 #endif
 
-#endif // BITCOIN_QT_WINSHUTDOWNMONITOR_H
+#endif // AYCH_QT_WINSHUTDOWNMONITOR_H
