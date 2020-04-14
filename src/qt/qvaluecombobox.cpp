e63d9306d370726f732f9e60f38c56693728912f
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-14 14:07:35 +0700

    Update qvaluecombobox.cpp

--- src/qt/qvaluecombobox.cpp
+++ src/qt/qvaluecombobox.cpp
@@ -1,6 +1,8 @@
-// Copyright (c) 2011-2018 The Bitcoin Core developers
+// Copyright (c) 2015 - 2020 Jean Wallet
+// Copyright (c) 2015 - 2020 The AYCHDeveloper.
 // Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or https://www.gnu.org/licenses.
 
 #include <qt/qvaluecombobox.h>
 
