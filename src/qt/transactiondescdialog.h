7209baee7992bf04ef09b1a32824c658327f8f9c
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-14 15:24:18 +0700

    Update transactiondescdialog.h

--- src/qt/transactiondescdialog.h
+++ src/qt/transactiondescdialog.h
@@ -1,9 +1,11 @@
-// Copyright (c) 2011-2014 The Bitcoin Core developers
+// Copyright (c) 2015 - 2020 Jean Wallet
+// Copyright (c) 2015 - 2020 The AYCHDeveloper.
 // Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or https://www.gnu.org/licenses.
 
-#ifndef BITCOIN_QT_TRANSACTIONDESCDIALOG_H
-#define BITCOIN_QT_TRANSACTIONDESCDIALOG_H
+#ifndef AYCH_QT_TRANSACTIONDESCDIALOG_H
+#define AYCH_QT_TRANSACTIONDESCDIALOG_H
 
 #include <QDialog>
 
@@ -28,4 +30,4 @@ private:
     Ui::TransactionDescDialog *ui;
 };
 
-#endif // BITCOIN_QT_TRANSACTIONDESCDIALOG_H
+#endif // AYCH_QT_TRANSACTIONDESCDIALOG_H
