db619b819b3f79d89db6811c23fae1735a62d2a6
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-14 14:36:18 +0700

    Update rpcconsole.cpp

--- src/qt/rpcconsole.cpp
+++ src/qt/rpcconsole.cpp
@@ -1,9 +1,11 @@
-// Copyright (c) 2011-2018 The Bitcoin Core developers
+// Copyright (c) 2015 - 2020 Jean Wallet
+// Copyright (c) 2015 - 2020 The AYCHDeveloper.
 // Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or https://www.gnu.org/licenses.
 
 #if defined(HAVE_CONFIG_H)
-#include <config/bitcoin-config.h>
+#include <config/aych-config.h>
 #endif
 
 #include <qt/rpcconsole.h>
