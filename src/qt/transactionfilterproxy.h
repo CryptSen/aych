eae62333b28fe02fe8638115a81321729560a37d
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-14 15:27:25 +0700

    Update transactionfilterproxy.h

--- src/qt/transactionfilterproxy.h
+++ src/qt/transactionfilterproxy.h
@@ -1,9 +1,11 @@
-// Copyright (c) 2011-2018 The Bitcoin Core developers
+// Copyright (c) 2015 - 2020 Jean Wallet
+// Copyright (c) 2015 - 2020 The AYCHDeveloper.
 // Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or https://www.gnu.org/licenses.
 
-#ifndef BITCOIN_QT_TRANSACTIONFILTERPROXY_H
-#define BITCOIN_QT_TRANSACTIONFILTERPROXY_H
+#ifndef AYCH_QT_TRANSACTIONFILTERPROXY_H
+#define AYCH_QT_TRANSACTIONFILTERPROXY_H
 
 #include <amount.h>
 
@@ -65,4 +67,4 @@ private:
     bool showInactive;
 };
 
-#endif // BITCOIN_QT_TRANSACTIONFILTERPROXY_H
+#endif // AYCH_QT_TRANSACTIONFILTERPROXY_H
