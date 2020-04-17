24422b8674ecb458f822859af7704d5934452d4b
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-17 14:41:58 +0700

    Update aych_nb.ts

--- src/qt/locale/aych_nb.ts
+++ src/qt/locale/aych_nb.ts
@@ -240,7 +240,7 @@
     </message>
 </context>
 <context>
-    <name>BitcoinGUI</name>
+    <name>AychGUI</name>
     <message>
         <source>Sign &amp;message...</source>
         <translation>Signer &amp;melding...</translation>
