a5973845b2014b525ea6ff9b761add8152f0a0a5
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-23 13:53:59 +0700

    Update zmqrpc.h

--- src/zmq/zmqrpc.h
+++ src/zmq/zmqrpc.h
@@ -1,12 +1,13 @@
-// Copyright (c) 2018 The Bitcoin Core developers
-// Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// Copyright (c) 2015 - 2020 The AYCHDeveloper
+// Distributed under the MIT software license, the AGPL-3.0+, see the accompanying
+// file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or https://www.gnu.org/licenses.
 
-#ifndef BITCOIN_ZMQ_ZMQRPC_H
-#define BITCOIN_ZMQ_ZMQRPC_H
+#ifndef AYCH_ZMQ_ZMQRPC_H
+#define AYCH_ZMQ_ZMQRPC_H
 
 class CRPCTable;
 
 void RegisterZMQRPCCommands(CRPCTable& t);
 
-#endif // BITCOIN_ZMQ_ZMRRPC_H
+#endif // AYCH_ZMQ_ZMRRPC_H
