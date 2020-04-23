cb86876c7c68804782bde89e5a5c384038d3ef73
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-23 13:39:32 +0700

    Update fees.h

--- src/policy/fees.h
+++ src/policy/fees.h
@@ -1,9 +1,11 @@
-// Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2018 The Bitcoin Core developers
-// Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
-#ifndef BITCOIN_POLICY_FEES_H
-#define BITCOIN_POLICY_FEES_H
+// Copyright (c) 2015 - 2020 Jean Wallet
+// Copyright (c) 2015 - 2020 The AYCHDeveloper
+// Distributed under the MIT software license, the AGPL-3.0+, see the accompanying
+// file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+// file LICENSE or https://www.gnu.org/licenses.
+
+#ifndef AYCH_POLICY_FEES_H
+#define AYCH_POLICY_FEES_H
 
 #include <amount.h>
 #include <policy/feerate.h>
@@ -295,4 +297,4 @@ private:
     FastRandomContext insecure_rand;
 };
 
-#endif // BITCOIN_POLICY_FEES_H
+#endif // AYCH_POLICY_FEES_H
