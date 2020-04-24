ac162c558dafb4c52594cc3a02bce30d2fd47037
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-24 17:34:10 +0700

    Update feature_blocksdir.py

--- test/functional/feature_blocksdir.py
+++ test/functional/feature_blocksdir.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
-# Copyright (c) 2018 The Bitcoin Core developers
-# Distributed under the MIT software license, see the accompanying
-# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+# Copyright (c) 2015 - 2020 The AYCHDeveloper.
+# Distributed under the MIT software license, the AGPL-3.0+, see the accompanying
+# file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+# file LICENSE or https://www.gnu.org/licenses.
+
 """Test the blocksdir option.
 """
 
 import os
 import shutil
 
-from test_framework.test_framework import BitcoinTestFramework, initialize_datadir
+from test_framework.test_framework import AychTestFramework, initialize_datadir
 
 
-class BlocksdirTest(BitcoinTestFramework):
+class BlocksdirTest(AychTestFramework):
     def set_test_params(self):
         self.setup_clean_chain = True
         self.num_nodes = 1
