cc91fd0cba6681e6f317cdf0dd586f6d4534aaf2
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-24 20:05:24 +0700

    Update mining_prioritisetransaction.py

--- test/functional/mining_prioritisetransaction.py
+++ test/functional/mining_prioritisetransaction.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
-# Copyright (c) 2015-2018 The Bitcoin Core developers
-# Distributed under the MIT software license, see the accompanying
-# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+# Copyright (c) 2015 - 2020 The AYCHDeveloper.
+# Distributed under the MIT software license, the AGPL-3.0+, see the accompanying
+# file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+# file LICENSE or https://www.gnu.org/licenses.
+
 """Test the prioritisetransaction mining RPC."""
 
 import time
 
 from test_framework.messages import COIN, MAX_BLOCK_BASE_SIZE
-from test_framework.test_framework import BitcoinTestFramework
+from test_framework.test_framework import AychTestFramework
 from test_framework.util import assert_equal, assert_raises_rpc_error, create_confirmed_utxos, create_lots_of_big_transactions, gen_return_txouts
 
-class PrioritiseTransactionTest(BitcoinTestFramework):
+class PrioritiseTransactionTest(AychTestFramework):
     def set_test_params(self):
         self.setup_clean_chain = True
         self.num_nodes = 2
