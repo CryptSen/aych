f80080f71d3082c73da81ce5589df7b2b720b5c1
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-24 21:05:04 +0700

    Update rpc_bind.py

--- test/functional/rpc_bind.py
+++ test/functional/rpc_bind.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
-# Copyright (c) 2014-2018 The Bitcoin Core developers
-# Distributed under the MIT software license, see the accompanying
-# file COPYING or http://www.opensource.org/licenses/mit-license.php.
-"""Test running litecoind with the -rpcbind and -rpcallowip options."""
+# Copyright (c) 2015 - 2020 The AYCHDeveloper.
+# Distributed under the MIT software license, the AGPL-3.0+, see the accompanying
+# file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+# file LICENSE or https://www.gnu.org/licenses.
+
+"""Test running aychd with the -rpcbind and -rpcallowip options."""
 
 import sys
 
 from test_framework.netutil import all_interfaces, addr_to_hex, get_bind_addrs, test_ipv6_local
-from test_framework.test_framework import BitcoinTestFramework, SkipTest
+from test_framework.test_framework import AychTestFramework, SkipTest
 from test_framework.util import assert_equal, assert_raises_rpc_error, get_rpc_proxy, rpc_port, rpc_url
 
-class RPCBindTest(BitcoinTestFramework):
+class RPCBindTest(AychTestFramework):
     def set_test_params(self):
         self.setup_clean_chain = True
         self.bind_to_localhost_only = False
