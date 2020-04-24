f9ad0301ee0dd4ac556446193da9ddbfa0607b7b
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-24 19:52:32 +0700

    Update mempool_reorg.py

--- test/functional/mempool_reorg.py
+++ test/functional/mempool_reorg.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
-# Copyright (c) 2014-2018 The Bitcoin Core developers
-# Distributed under the MIT software license, see the accompanying
-# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+# Copyright (c) 2015 - 2020 The AYCHDeveloper.
+# Distributed under the MIT software license, the AGPL-3.0+, see the accompanying
+# file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+# file LICENSE or https://www.gnu.org/licenses.
+
 """Test mempool re-org scenarios.
 
 Test re-org scenarios with a mempool that contains transactions
@@ -9,11 +11,11 @@ that spend (directly or indirectly) coinbase transactions.
 """
 
 from test_framework.blocktools import create_raw_transaction
-from test_framework.test_framework import BitcoinTestFramework
+from test_framework.test_framework import AychTestFramework
 from test_framework.util import assert_equal, assert_raises_rpc_error
 
 
-class MempoolCoinbaseTest(BitcoinTestFramework):
+class MempoolCoinbaseTest(AychTestFramework):
     def set_test_params(self):
         self.num_nodes = 2
 
