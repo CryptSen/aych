b2859311adfa28afa88e33a5cc395f80887c7940
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-25 07:34:32 +0700

    Update rpc_help.py

--- test/functional/rpc_help.py
+++ test/functional/rpc_help.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
-# Copyright (c) 2018 The Bitcoin Core developers
-# Distributed under the MIT software license, see the accompanying
-# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+# Copyright (c) 2015 - 2020 The AYCHDeveloper.
+# Distributed under the MIT software license, the AGPL-3.0+, see the accompanying
+# file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+# file LICENSE or https://www.gnu.org/licenses.
+
 """Test RPC help output."""
 
-from test_framework.test_framework import BitcoinTestFramework
+from test_framework.test_framework import AychTestFramework
 from test_framework.util import assert_equal, assert_raises_rpc_error
 
 import os
 
 
-class HelpRpcTest(BitcoinTestFramework):
+class HelpRpcTest(AychTestFramework):
     def set_test_params(self):
         self.num_nodes = 1
 
