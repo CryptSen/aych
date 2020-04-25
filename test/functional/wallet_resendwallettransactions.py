21f0ee9359887780cbca8934c4f62afbf698ec04
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-25 09:50:18 +0700

    Update wallet_resendwallettransactions.py

--- test/functional/wallet_resendwallettransactions.py
+++ test/functional/wallet_resendwallettransactions.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
-# Copyright (c) 2017-2018 The Bitcoin Core developers
-# Distributed under the MIT software license, see the accompanying
-# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+# Copyright (c) 2015 - 2020 The AYCHDeveloper.
+# Distributed under the MIT software license, the AGPL-3.0+, see the accompanying
+# file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+# file LICENSE or https://www.gnu.org/licenses.
+
 """Test resendwallettransactions RPC."""
 
-from test_framework.test_framework import BitcoinTestFramework
+from test_framework.test_framework import AychTestFramework
 from test_framework.util import assert_equal, assert_raises_rpc_error
 
-class ResendWalletTransactionsTest(BitcoinTestFramework):
+class ResendWalletTransactionsTest(AychTestFramework):
     def set_test_params(self):
         self.num_nodes = 1
         self.extra_args = [['--walletbroadcast=false']]
