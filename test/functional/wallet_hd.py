ef8ff38f4e36a77bf8b8451502dbbecf00fb6099
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-25 09:29:49 +0700

    Update wallet_hd.py

--- test/functional/wallet_hd.py
+++ test/functional/wallet_hd.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
-# Copyright (c) 2016-2018 The Bitcoin Core developers
-# Distributed under the MIT software license, see the accompanying
-# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+# Copyright (c) 2015 - 2020 The AYCHDeveloper.
+# Distributed under the MIT software license, the AGPL-3.0+, see the accompanying
+# file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+# file LICENSE or https://www.gnu.org/licenses.
+
 """Test Hierarchical Deterministic wallet function."""
 
 import os
 import shutil
 
-from test_framework.test_framework import BitcoinTestFramework
+from test_framework.test_framework import AychTestFramework
 from test_framework.util import (
     assert_equal,
     connect_nodes_bi,
@@ -15,7 +17,7 @@ from test_framework.util import (
 )
 
 
-class WalletHDTest(BitcoinTestFramework):
+class WalletHDTest(AychTestFramework):
     def set_test_params(self):
         self.setup_clean_chain = True
         self.num_nodes = 2
