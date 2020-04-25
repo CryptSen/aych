43f51600326b717cca908d5cef4ddca8e9566d6f
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-25 07:33:33 +0700

    Update rpc_getchaintips.py

--- test/functional/rpc_getchaintips.py
+++ test/functional/rpc_getchaintips.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
-# Copyright (c) 2014-2018 The Bitcoin Core developers
-# Distributed under the MIT software license, see the accompanying
-# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+# Copyright (c) 2015 - 2020 The AYCHDeveloper.
+# Distributed under the MIT software license, the AGPL-3.0+, see the accompanying
+# file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+# file LICENSE or https://www.gnu.org/licenses.
+
 """Test the getchaintips RPC.
 
 - introduce a network split
@@ -10,10 +12,10 @@
 - verify that getchaintips now returns two chain tips.
 """
 
-from test_framework.test_framework import BitcoinTestFramework
+from test_framework.test_framework import AychTestFramework
 from test_framework.util import assert_equal
 
-class GetChainTipsTest (BitcoinTestFramework):
+class GetChainTipsTest (AychTestFramework):
     def set_test_params(self):
         self.num_nodes = 4
 
