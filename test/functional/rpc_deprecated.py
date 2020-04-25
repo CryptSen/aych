899da05938877f7b591b663aeb9eee19d4bed409
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-25 07:26:21 +0700

    Update rpc_deprecated.py

--- test/functional/rpc_deprecated.py
+++ test/functional/rpc_deprecated.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
-# Copyright (c) 2017-2018 The Bitcoin Core developers
-# Distributed under the MIT software license, see the accompanying
-# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+# Copyright (c) 2015 - 2020 The AYCHDeveloper.
+# Distributed under the MIT software license, the AGPL-3.0+, see the accompanying
+# file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+# file LICENSE or https://www.gnu.org/licenses.
+
 """Test deprecation of RPC calls."""
-from test_framework.test_framework import BitcoinTestFramework
+from test_framework.test_framework import AychTestFramework
 from test_framework.util import assert_raises_rpc_error
 
-class DeprecatedRpcTest(BitcoinTestFramework):
+class DeprecatedRpcTest(AychTestFramework):
     def set_test_params(self):
         self.num_nodes = 2
         self.setup_clean_chain = True
