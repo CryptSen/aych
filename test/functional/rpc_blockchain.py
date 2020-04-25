5d265f8e5b5d41be37eecd179a61a391c46b4b63
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-25 07:19:38 +0700

    Update rpc_blockchain.py

--- test/functional/rpc_blockchain.py
+++ test/functional/rpc_blockchain.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
-# Copyright (c) 2014-2018 The Bitcoin Core developers
-# Distributed under the MIT software license, see the accompanying
-# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+# Copyright (c) 2015 - 2020 The AYCHDeveloper.
+# Distributed under the MIT software license, the AGPL-3.0+, see the accompanying
+# file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+# file LICENSE or https://www.gnu.org/licenses.
+
 """Test RPCs related to blockchainstate.
 
 Test the following RPCs:
@@ -22,7 +24,7 @@ from decimal import Decimal
 import http.client
 import subprocess
 
-from test_framework.test_framework import BitcoinTestFramework
+from test_framework.test_framework import AychTestFramework
 from test_framework.util import (
     assert_equal,
     assert_greater_than,
@@ -44,7 +46,7 @@ from test_framework.mininode import (
 )
 
 
-class BlockchainTest(BitcoinTestFramework):
+class BlockchainTest(AychTestFramework):
     def set_test_params(self):
         self.num_nodes = 1
 
