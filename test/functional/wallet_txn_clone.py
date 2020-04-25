f861e27f47b8fd3d62f06a58e3f4c3d430e4c565
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-25 09:53:56 +0700

    Update wallet_txn_clone.py

--- test/functional/wallet_txn_clone.py
+++ test/functional/wallet_txn_clone.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
-# Copyright (c) 2014-2018 The Bitcoin Core developers
-# Distributed under the MIT software license, see the accompanying
-# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+# Copyright (c) 2015 - 2020 The AYCHDeveloper.
+# Distributed under the MIT software license, the AGPL-3.0+, see the accompanying
+# file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+# file LICENSE or https://www.gnu.org/licenses.
+
 """Test the wallet accounts properly when there are cloned transactions with malleated scriptsigs."""
 
-from test_framework.test_framework import BitcoinTestFramework
+from test_framework.test_framework import AychTestFramework
 from test_framework.util import (
     assert_equal,
     connect_nodes,
@@ -12,7 +14,7 @@ from test_framework.util import (
     sync_blocks,
 )
 
-class TxnMallTest(BitcoinTestFramework):
+class TxnMallTest(AychTestFramework):
     def set_test_params(self):
         self.num_nodes = 4
 
@@ -37,7 +39,7 @@ class TxnMallTest(BitcoinTestFramework):
         else:
             output_type = "legacy"
 
-        # All nodes should start with 1,250 BTC:
+        # All nodes should start with 1,250 AEX, AYCH:
         starting_balance = 1250
         for i in range(4):
             assert_equal(self.nodes[i].getbalance(), starting_balance)
@@ -95,7 +97,7 @@ class TxnMallTest(BitcoinTestFramework):
         tx1 = self.nodes[0].gettransaction(txid1)
         tx2 = self.nodes[0].gettransaction(txid2)
 
-        # Node0's balance should be starting balance, plus 50BTC for another
+        # Node0's balance should be starting balance, plus 50AEX, AYCH for another
         # matured block, minus tx1 and tx2 amounts, and minus transaction fees:
         expected = starting_balance + node0_tx1["fee"] + node0_tx2["fee"]
         if self.options.mine_block:
@@ -138,7 +140,7 @@ class TxnMallTest(BitcoinTestFramework):
         assert_equal(tx1_clone["confirmations"], 2)
         assert_equal(tx2["confirmations"], 1)
 
-        # Check node0's total balance; should be same as before the clone, + 100 BTC for 2 matured,
+        # Check node0's total balance; should be same as before the clone, + 100 AEX, AYCH for 2 matured,
         # less possible orphaned matured subsidy
         expected += 100
         if (self.options.mine_block):
