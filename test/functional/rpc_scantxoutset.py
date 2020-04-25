be55afe3f1d6370ebbdff6375f8bfc66d33ab283
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-25 07:44:22 +0700

    Update rpc_scantxoutset.py

--- test/functional/rpc_scantxoutset.py
+++ test/functional/rpc_scantxoutset.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
-# Copyright (c) 2018 The Bitcoin Core developers
-# Distributed under the MIT software license, see the accompanying
-# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+# Copyright (c) 2015 - 2020 The AYCHDeveloper.
+# Distributed under the MIT software license, the AGPL-3.0+, see the accompanying
+# file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+# file LICENSE or https://www.gnu.org/licenses.
+
 """Test the scantxoutset rpc call."""
-from test_framework.test_framework import BitcoinTestFramework
+from test_framework.test_framework import AychTestFramework
 from test_framework.util import assert_equal
 
 from decimal import Decimal
 import shutil
 import os
 
-class ScantxoutsetTest(BitcoinTestFramework):
+class ScantxoutsetTest(AychTestFramework):
     def set_test_params(self):
         self.num_nodes = 1
         self.setup_clean_chain = True
