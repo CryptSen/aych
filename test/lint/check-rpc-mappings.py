0ca1af70066f675091449f93b8207fdc76a04a88
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-25 11:08:53 +0700

    Update check-rpc-mappings.py

--- test/lint/check-rpc-mappings.py
+++ test/lint/check-rpc-mappings.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
-# Copyright (c) 2017-2018 The Bitcoin Core developers
-# Distributed under the MIT software license, see the accompanying
-# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+# Copyright (c) 2015 - 2020 Jean Wallet
+# Copyright (c) 2015 - 2020 The AYCHDeveloper.
+# Distributed under the MIT software license, the AGPL-3.0+, see the accompanying
+# file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+# file LICENSE or https://www.gnu.org/licenses.
+
 """Check RPC argument consistency."""
 
 from collections import defaultdict
