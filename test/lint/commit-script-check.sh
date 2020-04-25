cc90a7a70281c0c8939fde7082949d1f5a005a20
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-25 11:09:32 +0700

    Update commit-script-check.sh

--- test/lint/commit-script-check.sh
+++ test/lint/commit-script-check.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-# Copyright (c) 2017 The Bitcoin Core developers
-# Distributed under the MIT software license, see the accompanying
-# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+# Copyright (c) 2015 - 2020 Jean Wallet
+# Copyright (c) 2015 - 2020 The AYCHDeveloper.
+# Distributed under the MIT software license, the AGPL-3.0+, see the accompanying
+# file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+# file LICENSE or https://www.gnu.org/licenses.
 
 # This simple script checks for commits beginning with: scripted-diff:
 # If found, looks for a script between the lines -BEGIN VERIFY SCRIPT- and
